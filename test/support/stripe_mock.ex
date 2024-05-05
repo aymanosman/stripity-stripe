8534a14536ff6486118c6069b573b625a05ee267
aymanosman <aymano.osman@gmail.com>
2024-05-06 00:57:49 +0900

    Revert "use erlexec directly and remove exexec"
    This reverts commit b53fb8f84cd415536ec06711b242a01f39871764.

--- test/support/stripe_mock.ex
+++ test/support/stripe_mock.ex
@@ -80,13 +80,13 @@ defmodule Stripe.StripeMock do
       end
 
     Logger.debug("Starting stripe_mock on port #{port}")
-    :exec.run([executable | port_args], [:monitor, :stdout, :stderr])
+    Exexec.run([executable | port_args], monitor: true, stdout: true, stderr: true)
   end
 
   defp kill_stripe_mock(manager_pid) do
     Logger.debug("Killing stripe_mock")
 
-    case :exec.stop(manager_pid) do
+    case Exexec.stop(manager_pid) do
       :ok ->
         :ok
 
