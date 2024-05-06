7e7320235feeb00252b4773f9359eafbd9128716
aymanosman <aymano.osman@gmail.com>
2024-05-06 18:41:14 +0900

    -user root

--- config/config.exs
+++ config/config.exs
@@ -3,3 +3,7 @@ import Config
 if File.exists?("config/config.secret.exs") do
   import_config "config.secret.exs"
 end
+
+config :erlexec,
+  # erlexec is very particular about being run as root
+  user: System.get_env("USER")
